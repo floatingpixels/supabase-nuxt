b7b9d0cef278ad5efa018e9f55d164d9064a2a2b
Aietes <stefan@standa.de>
2024-01-24 14:24:33 +0100

    Added server api call to playground, logout button

--- playground/pages/index.vue
+++ playground/pages/index.vue
@@ -4,15 +4,31 @@ const supabase = useSupabaseClient()
 //   data: { user },
 // } = await supabase.auth.getUser()
 const user = await useSupabaseUser()
+
+const { data: clientData } = await supabase.from('test').select('*')
+const { data: serverData } = await useFetch('/api/test')
+
+const signOut = async () => {
+  const { error } = await supabase.auth.signOut()
+  if (error) console.log(error)
+}
 </script>
 <template>
   <div>
     <div>
-      <div>User</div>
-      <div>{{ user }}</div>
+      <h2>User</h2>
+      <div>User ID:{{ user?.id }}</div>
+      <div>E-Mail: {{ user?.email }}</div>
+      <button @click="signOut">Sign out</button>
     </div>
     <div>
-      <div>Supabase Client</div>
+      <h2>Data</h2>
+      <div>
+        <div>Data from browser query (client side)</div>
+        <pre>{{ clientData }}</pre>
+      </div>
+      <div>Data from server API (server side)</div>
+      <pre>{{ serverData }}</pre>
     </div>
   </div>
 </template>
