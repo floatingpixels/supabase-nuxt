34fb4d73e6346b6c6b68f02be04b2c1a3a24875e
Aietes <stefan@standa.de>
2024-07-01 17:00:22 +0200

    Secure check for user on the sever instead of from session

--- src/runtime/composables/useSupabaseUser.ts
+++ src/runtime/composables/useSupabaseUser.ts
@@ -2,14 +2,6 @@ import { useSupabaseClient } from './useSupabaseClient'
 
 export const useSupabaseUser = async () => {
   const supabase = useSupabaseClient()
-
-  const {
-    data: { session },
-  } = await supabase.auth.getSession()
-
-  if (!session?.user) {
-    return null
-  }
-
-  return session.user
+  const { data: user } = await supabase.auth.getUser()
+  return user.user
 }
