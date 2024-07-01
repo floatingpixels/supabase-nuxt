adf416b790666c370c1790e0f6a08b4f5133a2e0
Aietes <stefan@standa.de>
2024-07-01 17:06:27 +0200

    Move to updated eslint config

---
+++ eslint.config.js
@@ -0,0 +1,14 @@
+import { createConfigForNuxt } from '@nuxt/eslint-config/flat'
+import eslintConfigPrettier from 'eslint-config-prettier'
+
+export default createConfigForNuxt({})
+  .override('nuxt/vue/rules', {
+    rules: {
+      'vue/no-setup-props-destructure': 'off',
+      'vue/multi-word-component-names': 'off',
+    },
+  })
+  .append(eslintConfigPrettier)
+  .append({
+    ignores: ['dist', 'node_modules'],
+  })
