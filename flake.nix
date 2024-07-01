510d95ad6f5fdc7ce822f3326785f776aee748a3
Aietes <stefan@standa.de>
2024-07-01 15:18:45 +0200

    Adding nix env

---
+++ flake.nix
@@ -0,0 +1,27 @@
+{
+  description = "Development environment for Nuxt.js project with Node.js and TypeScript";
+
+  inputs = {
+    nixpkgs.url = "nixpkgs/nixos-24.05";
+    # nixpkgs-unstable.url = "github:nixos/nixpkgs/nixos-unstable";
+    flake-utils.url = "github:numtide/flake-utils";
+  };
+
+  outputs = { self, nixpkgs, flake-utils }:
+    flake-utils.lib.eachDefaultSystem (system:
+      let
+        # pkgs = nixpkgs.legacyPackages.${system};
+        pkgs = import nixpkgs { inherit system; };
+      in
+      {
+        devShell = pkgs.mkShell {
+          buildInputs = with pkgs; [
+            nodejs_22 # Node.js
+            nodePackages.pnpm # PNPM package manager
+            nodePackages."@antfu/ni"
+            typescript
+          ];
+        };
+      }
+    );
+}
