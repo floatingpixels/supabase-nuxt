bcb3ac7fc3260eb4b567f4890b47b5880b0c14be
Aietes <stefan@standa.de>
2024-12-13 17:09:03 +0100

    refactor(middleware): simplify regexp in middleware exclude

--- src/runtime/plugins/middleware-auth-redirect.ts
+++ src/runtime/plugins/middleware-auth-redirect.ts
@@ -12,10 +12,9 @@ export default defineNuxtPlugin({
         const { login, exclude } = config.redirectOptions
 
         // Do not redirect on login route and excluded routes
-        const isExcluded = [...(exclude || []), login ? login : '/login']?.some(path => {
-          const regex = new RegExp(`^${path.replace(/\*/g, '.*')}$`)
-          return regex.test(to.path)
-        })
+        const isExcluded = [...(exclude || []), login || '/login'].some(path =>
+          new RegExp(`^${path.replace(/\*/g, '.*')}$`).test(to.path),
+        )
         if (isExcluded) return
 
         const user = await useSupabaseUser()
